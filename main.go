f59f363932f6a4b6c7a181065fc158dfbcd39b00
liukai6666 <liukai20216666@163.com>
2021-06-18 20:42:53 +0800

    addition

--- main.go
+++ main.go
@@ -1,12 +1,18 @@
 package main
+
 import (
 	"fmt"
 )
 
 func sayHello(name string) string {
-	return "Hello "+name
+	return "Hello " + name
 }
 
 func main() {
-	fmt.Println(sayHello("liukai"))
+	//fmt.Println(sayHello("liukai"))
+	fmt.Println(addition(1, 2))
+}
+
+func addition(x int, y int) int {
+	return x + y
 }
