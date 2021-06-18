ead8f606ba7da03c77a0493aa664b18117863a99
liukai6666 <liukai20216666@163.com>
2021-06-18 21:27:29 +0800

    reflect demo 检测变量类型

---
+++ variableArray.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	var beatles [4]string
+	beatles[0] = "John"
+	beatles[1] = "Paul"
+	beatles[2] = "Ringo"
+	beatles[3] = "George"
+
+	fmt.Println(beatles[2])
+}
