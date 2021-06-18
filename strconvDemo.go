6a87a3df0fd9731c711f77b185721c48dc97adfe
liukai6666 <liukai20216666@163.com>
2021-06-18 21:39:30 +0800

    strconv demo 添加注释

--- strconvDemo.go
+++ strconvDemo.go
@@ -9,9 +9,11 @@ import (
 func main() {
 
 	var s string = "true"
+	//将 string true 转换为 boolean true
 	b, _ := strconv.ParseBool(s)
 	fmt.Println(b)
 
+	//将 boolean true 转换为 string true
 	boolString := strconv.FormatBool(b)
 	fmt.Println("value ", boolString)
 	fmt.Println("type ", reflect.TypeOf(boolString))
