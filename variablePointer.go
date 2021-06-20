b673ecd19fa4de42c00da527baf93370892ca245
liukai6666 <liukai20216666@163.com>
2021-06-20 14:33:19 +0800

    增加传值例子

--- variablePointer.go
+++ variablePointer.go
@@ -9,7 +9,21 @@ import (
 
 func main() {
 
-	s := "a"
-	//变量在内存中的地址是：  0xc00008e1e0
-	fmt.Println("变量在内存中的地址是： ", &s)
+	/*
+		s := "a"
+		//变量在内存中的地址是：  0xc00008e1e0
+		fmt.Println("变量在内存中的地址是： ", &s)
+	*/
+
+	i := 1
+	//i address is : 0xc0000b2008
+	fmt.Println("i address is :", &i)
+	//传值、内存新开辟空间
+	showMemoryAddress(i)
+}
+
+func showMemoryAddress(x int) {
+	//x address is : 0xc0000b2020
+	fmt.Println("x address is :", &x)
+	return
 }
