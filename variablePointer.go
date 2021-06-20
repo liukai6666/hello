9eb29e6c193dd7c26c18bb360175185d2ff215dd
liukai6666 <liukai20216666@163.com>
2021-06-20 15:05:32 +0800

    指针例子 增加参数定义接收指针使用*,访问指针指向值，在访问时在变量前加*

--- variablePointer.go
+++ variablePointer.go
@@ -1,5 +1,8 @@
 /*
-指针：变量在内存中的地址，可在变量名前加上&字符。
+指针：变量在内存中的地址
+使用指针变量名前加&  fmt.Println(&x)
+声明指针，在变量名前加*  func transferPointer(x int*)
+非参数声明指针部分、要访问指针地址指向的值，需要在变量前面加*号  x = pointer, fmt.Println(*x)
 */
 package main
 
@@ -20,10 +23,20 @@ func main() {
 	fmt.Println("i address is :", &i)
 	//传值、内存新开辟空间
 	showMemoryAddress(i)
+	//传址
+	showMemoryAddressParameterPointer(&i)
 }
 
 func showMemoryAddress(x int) {
-	//x address is : 0xc0000b2020
-	fmt.Println("x address is :", &x)
+	//transfer value, x address is : 0xc0000b2020
+	fmt.Println("transfer value, x address is :", &x)
+	return
+}
+
+func showMemoryAddressParameterPointer(x *int) {
+
+	//参数接收的就是地址、此时访问x就是访问地址、不需要再加&
+	//transfer pointer, x address is : 0xc0000b2008
+	fmt.Println("transfer pointer, x address is :", x)
 	return
 }
